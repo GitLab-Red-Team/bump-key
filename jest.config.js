cd20c93132c1bd58eae5859c6bebfaa0e7d3ea7d
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-12-22 14:29:35 -0500

    configure jest testing patterns and reorg

--- jest.config.js
+++ jest.config.js
@@ -150,11 +150,18 @@ module.exports = {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
-
+testMatch: [
+  "**/src/**/*spec.[jt]s?(x)"
+],
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
   // ],
+testPathIgnorePatterns: [
+  "/node_modules/",
+  "/bin/"
+]
+
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
