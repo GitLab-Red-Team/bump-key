4651a0c846360f8f42bce4624d4512f84d08b730
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-02-23 09:33:16 -0500

    consitant quoting

--- src/file-io/reader.js
+++ src/file-io/reader.js
@@ -2,8 +2,8 @@ import readJson from 'read-package-json';
 
 export const readPackageJSON = (path) => {
     return new Promise((resolve, reject) => {
-        if (path === undefined || path === "")
-            reject("No path was given!");
+        if (path === undefined || path === '')
+            reject('No path was given!');
         readJson(path, null, true, (err, data) => {
             if (err) return reject(err);
             resolve(data);
