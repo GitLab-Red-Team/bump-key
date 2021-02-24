f5c503c4e738658ef36519421d1418faea0ca539
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-02-24 18:26:20 -0500

    use a default prefix param for logging

--- src/out/index.js
+++ src/out/index.js
@@ -1,13 +1,13 @@
 import chalk from "chalk";
 
-export const info = (txt) => {
-    console.log(chalk`[*] {green ${txt}}`);
+export const info = (txt, prefix = '[*]') => {
+    console.log(chalk`${prefix} {green ${txt}}`);
 };
 
-export const warn = (txt) => {
-    console.log(chalk`[-] {orange ${txt}}`);
+export const warn = (txt, prefix = '[-]') => {
+    console.log(chalk`${prefix} {orange ${txt}}`);
 };
 
-export const error = (txt) => {
-    console.log(chalk`[!] {red ${txt}}`);
+export const error = (txt, prefix = '[!]') => {
+    console.log(chalk`${prefix} {red ${txt}}`);
 };
