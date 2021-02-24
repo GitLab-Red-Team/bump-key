72c11ce1893fd5c4e5c99775ce898af5ec46f453
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-02-24 18:38:16 -0500

    add some basic recon output from the package.json

--- src/index.js
+++ src/index.js
@@ -13,7 +13,14 @@ let options = init.setOptions(yargs);
 
 reader.readPackageJSON(options.root)
     .then((packageJson) => {
+        let devDepCnt = Object.entries(packageJson.devDependencies).length;
+        let prodDepCnt = Object.entries(packageJson.dependencies).length;
+        let url = packageJson.repository.url;
         out.info(`Target: ${packageJson.name} v${packageJson.version}`);
+        url && out.info(`Repository URL: ${url}`);
+        out.info(`Production Dependencies: ${prodDepCnt}`);
+        out.info(`Development Dependencies: ${devDepCnt}`);
+        console.log(packageJson);
     }).catch((err) => {
         throw err;
     });
