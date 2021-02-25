39c80fa7f6da2c1d3b8effc71963e84f76a95682
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-02-25 18:29:14 -0500

    dump raw dep output

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as init from './initialization/init';
 import * as out from './out';
+import * as dependencies from './dependencies';
 
 import yargs from 'yargs';
 
@@ -8,4 +9,13 @@ process.on('unhandledRejection', (reason) => {
     out.error(`Unhandled promise rejection: ${reason}`);
 });
 
-let options = init.setOptions(yargs);
+let cmdOptions = init.setOptions(yargs);
+
+let depOptions = {
+    cwd: cmdOptions.root
+};
+
+dependencies.recon(depOptions)
+    .then((deps) => {
+        console.log(deps);
+    });
