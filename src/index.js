0c4a7d3cd32105beea8e82fd2691387db6a5d9cf
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-03-12 16:16:32 -0500

    add a goofy banner and a versioning placeholder

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import init from './initialization/index.js';
 import out from './out/index.js';
 import dependencies from './dependencies/index.js';
+import { BANNER } from './constants/index.js';
 
 import yargs from 'yargs';
+import chalk from 'chalk';
 
 process.on('unhandledRejection', (reason) => {
     out.error(`Unhandled promise rejection: ${reason}`);
@@ -14,6 +16,9 @@ let cmdOptions = init.setOptions(yargs);
 let depOptions = {
     cwd: cmdOptions.root
 };
+
+console.log(chalk.keyword('purple').bold(BANNER));
+console.log(chalk.keyword('purple')('v1.0.0 - GitLab Red Team'));
 
 dependencies.recon(depOptions)
     .then((deps) => {
