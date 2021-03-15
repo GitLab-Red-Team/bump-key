044f35ef2d0094b85f3560881ebfe35b58f7e3cc
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-03-15 15:17:20 -0400

    create / test npm view command

--- src/commands/index-spec.js
+++ src/commands/index-spec.js
@@ -2,13 +2,13 @@ import commands from './index.js';
 
 import {expect} from 'chai';
 
-describe('commands', () => {
+describe('commands - npm view', () => {
     let commandResults;
     beforeEach(async () => {
-        commandResults = await commands.npmls('npm@latest');
+        commandResults = await commands.npmView('chalk@latest');
     });
     afterEach(() => {});
     it('executes an arbitrary command async', () => {
-        expect(commandResults.length).not.to.eql(0);
+        expect(commandResults.devDependencies.length).not.to.eql(0);
     });
 });
--- src/commands/index.js
+++ src/commands/index.js
@@ -5,18 +5,18 @@ import { PATHS } from '../constants/index.js';
 const execShellCommand = (cmd) => {
     const exec = child_process.exec;
     return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
+        exec(`${PATHS.bin}/${cmd}`, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
             }
-            resolve(stdout? stdout : stderr);
+            resolve(stdout? JSON.parse(stdout) : stderr);
         });
     });
 }
 
 const commands = {
-    'npmls': (pkg) => {
-        return execShellCommand(`${PATHS.bin}/npm ls --json ${pkg}`);
+    'npmView': (pkg) => {
+        return execShellCommand(`npm view --json ${pkg}`);
     }
 };
 
