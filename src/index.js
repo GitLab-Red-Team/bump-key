47e2035e030eca6a8ab4e28045a3c9abd0b7268b
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-03-17 17:04:58 -0400

    ascii art shenanigans

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ let depOptions = {
 };
 const printBanner = () => {
     console.log(chalk.keyword('orange').bold(BANNER));
-    console.log(chalk.keyword('purple').bgKeyword('orange')('~~~ bump-key v0.0.1 - GitLab Red Team ~~~'));
+    console.log('   ' + chalk.keyword('purple').bgKeyword('orange')('   ~~~ bump-key v0.0.1 - GitLab Red Team ~~~   '));
     console.log();
 };
 
@@ -28,7 +28,10 @@ const processDependency = async (dep) => {
     let view = await commands.npmView(installed);
     out.info(`${installed} 
     * bump to latest: ${dep.bump}
-    * dev dependency: ${dep.devDependency}
+    * wanted: ${dep.packageWanted}
+    * latest version: ${dep.latest}
+    * url: ${dep.homepage ? dep.homepage : 'NA'}
+    * used in script: ${dep.usedInScripts ? dep.usedInScripts : false}
     * top-level dev dependencies: ${view.devDependencies ? Object.entries(view.devDependencies).length : 0}
     * top-level prod dependencies:  ${view.dependencies ? Object.entries(view.dependencies).length : 0}`
     );
