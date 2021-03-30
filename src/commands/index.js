131491c2c569c0b151cb477b5c1f6842b380c7ce
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-03-30 15:21:03 -0400

    simplify command exectuion

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,16 +1,14 @@
 /* eslint-disable camelcase */
+import util from 'util';
 import child_process from 'child_process';
 
-import { PATHS } from '../constants/index.js';
+const async_cp = util.promisify(child_process.exec)
 
-const execShellCommand = (cmd) => new Promise((resolve, reject) => {
-    child_process.exec(`${PATHS.bin}/${cmd}`, (error, stdout, stderr) => {
-        if (error) {
-            reject(error);
-        }
-        resolve(stdout ? JSON.parse(stdout) : stderr);
-    });
-});
+const execShellCommand = async (cmd) => {
+    const {stdout, stderr} = await async_cp(cmd);
+    if (stderr) throw stderr;
+    return JSON.parse(stdout);
+}
 
 const commands = {
     npmView: async (pkg) => execShellCommand(`npm view --json ${pkg}`),
