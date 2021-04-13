e3d9118b7668b24fcb568facccf6c9fa2723b1b9
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-04-13 16:45:37 -0400

    create/destroy fakePackages at the test suite level

--- src/dependencies/index-spec.js
+++ src/dependencies/index-spec.js
@@ -6,125 +6,130 @@ import {BUMP} from '../constants/index.js';
 import dependencies from './index.js';
 
 describe('dependendencies', () => {
+    let fakePackages;
+    beforeEach(() => {
+        /* eslint-disable max-len */
+        fakePackages = [
+        {
+            moduleName: 'lodash', // name of the module.
+            homepage: 'https://lodash.com/', // url to the home page.
+            regError: undefined, // error communicating with the registry
+            pkgError: undefined, // error reading the package.json
+            latest: '4.7.0', // latest according to the registry.
+            installed: '4.5.1', // version installed
+            isInstalled: true, // Is it installed?
+            notInstalled: false, // Is it installed?
+            packageWanted: '4.7.0', // Requested version from the package.json.
+            packageJson: '^4.6.1', // Version or range requested in the parent package.json.
+            devDependency: false, // Is this a devDependency?
+            usedInScripts: undefined, // Array of `scripts` in package.json that use this module.
+            mismatch: false, // Does the version installed not match the range in package.json?
+            semverValid: '4.6.1', // Is the installed version valid semver?
+            easyUpgrade: false, // Will running just `npm install` upgrade the module?
+            bump: 'minor', // What kind of bump is required to get the latest, such as patch, minor, major.
+            unused: false, // Is this module used in the code?
+        },
+        {
+            moduleName: 'babel',
+            homepage: 'https://babel.com',
+            regError: undefined,
+            pkgError: undefined,
+            latest: '9.1.0',
+            installed: '7.8.9',
+            isInstalled: true,
+            notInstalled: false,
+            packageJson: '^7.7.0',
+            devDependency: true,
+            usedInScripts: true,
+            mismatch: false,
+            semverValid: '7.8.9',
+            easyUpgrade: false,
+            bump: 'major',
+            unused: false,
+        },
+        {
+            moduleName: 'easy-upgrade',
+            homepage: 'https://easy.com',
+            regError: undefined,
+            pkgError: undefined,
+            latest: '1.1.1',
+            installed: '1.1.0',
+            isInstalled: true,
+            notInstalled: false,
+            packageJson: '^1.1.1',
+            devDependency: true,
+            usedInScripts: true,
+            mismatch: false,
+            semverValid: '1.1.1',
+            easyUpgrade: true,
+            bump: 'patch',
+            unused: false,
+        },
+        {
+            moduleName: 'patch-upgrade',
+            homepage: 'https://patch.com',
+            regError: undefined,
+            pkgError: undefined,
+            latest: '5.1.9',
+            installed: '5.1.0',
+            isInstalled: true,
+            notInstalled: false,
+            packageJson: '^5.1.0',
+            devDependency: false,
+            usedInScripts: true,
+            mismatch: false,
+            semverValid: '5.1.0',
+            easyUpgrade: false,
+            bump: 'patch',
+            unused: false,
+        },
+        {
+            moduleName: 'null-upgrade',
+            homepage: 'https://patch.com',
+            regError: undefined,
+            pkgError: undefined,
+            latest: '6.1.9',
+            installed: '6.1.9',
+            isInstalled: true,
+            notInstalled: false,
+            packageJson: '^6.1.9',
+            devDependency: false,
+            usedInScripts: true,
+            mismatch: false,
+            semverValid: '6.1.9',
+            easyUpgrade: false,
+            bump: null,
+            unused: false,
+        },
+        {
+            moduleName: 'invalid-semver',
+            homepage: 'https://semver-none.com',
+            regError: undefined,
+            pkgError: undefined,
+            latest: '0.0.62',
+            installed: '0.0.62',
+            isInstalled: true,
+            notInstalled: false,
+            packageJson: '^0.0.62',
+            devDependency: false,
+            usedInScripts: true,
+            mismatch: false,
+            semverValid: '6.1.9',
+            easyUpgrade: false,
+            bump: 'nonSemver',
+            unused: false,
+        }];
+    });
+    afterEach(() => {
+        fakePackages = undefined;
+    });
     describe('executeNpmCheck', () => {
         let deps;
         let checkerStub;
-        let fakePackages;
         const options = {
             cwd: process.cwd(),
         };
         beforeEach(async () => {
-            /* eslint-disable max-len */
-            fakePackages = [
-                {
-                    moduleName: 'lodash', // name of the module.
-                    homepage: 'https://lodash.com/', // url to the home page.
-                    regError: undefined, // error communicating with the registry
-                    pkgError: undefined, // error reading the package.json
-                    latest: '4.7.0', // latest according to the registry.
-                    installed: '4.5.1', // version installed
-                    isInstalled: true, // Is it installed?
-                    notInstalled: false, // Is it installed?
-                    packageWanted: '4.7.0', // Requested version from the package.json.
-                    packageJson: '^4.6.1', // Version or range requested in the parent package.json.
-                    devDependency: false, // Is this a devDependency?
-                    usedInScripts: undefined, // Array of `scripts` in package.json that use this module.
-                    mismatch: false, // Does the version installed not match the range in package.json?
-                    semverValid: '4.6.1', // Is the installed version valid semver?
-                    easyUpgrade: false, // Will running just `npm install` upgrade the module?
-                    bump: 'minor', // What kind of bump is required to get the latest, such as patch, minor, major.
-                    unused: false, // Is this module used in the code?
-                },
-                {
-                    moduleName: 'babel',
-                    homepage: 'https://babel.com',
-                    regError: undefined,
-                    pkgError: undefined,
-                    latest: '9.1.0',
-                    installed: '7.8.9',
-                    isInstalled: true,
-                    notInstalled: false,
-                    packageJson: '^7.7.0',
-                    devDependency: true,
-                    usedInScripts: true,
-                    mismatch: false,
-                    semverValid: '7.8.9',
-                    easyUpgrade: false,
-                    bump: 'major',
-                    unused: false,
-                },
-                {
-                    moduleName: 'easy-upgrade',
-                    homepage: 'https://easy.com',
-                    regError: undefined,
-                    pkgError: undefined,
-                    latest: '1.1.1',
-                    installed: '1.1.0',
-                    isInstalled: true,
-                    notInstalled: false,
-                    packageJson: '^1.1.1',
-                    devDependency: true,
-                    usedInScripts: true,
-                    mismatch: false,
-                    semverValid: '1.1.1',
-                    easyUpgrade: true,
-                    bump: 'patch',
-                    unused: false,
-                },
-                {
-                    moduleName: 'patch-upgrade',
-                    homepage: 'https://patch.com',
-                    regError: undefined,
-                    pkgError: undefined,
-                    latest: '5.1.9',
-                    installed: '5.1.0',
-                    isInstalled: true,
-                    notInstalled: false,
-                    packageJson: '^5.1.0',
-                    devDependency: false,
-                    usedInScripts: true,
-                    mismatch: false,
-                    semverValid: '5.1.0',
-                    easyUpgrade: false,
-                    bump: 'patch',
-                    unused: false,
-                },
-                {
-                    moduleName: 'null-upgrade',
-                    homepage: 'https://patch.com',
-                    regError: undefined,
-                    pkgError: undefined,
-                    latest: '6.1.9',
-                    installed: '6.1.9',
-                    isInstalled: true,
-                    notInstalled: false,
-                    packageJson: '^6.1.9',
-                    devDependency: false,
-                    usedInScripts: true,
-                    mismatch: false,
-                    semverValid: '6.1.9',
-                    easyUpgrade: false,
-                    bump: null,
-                    unused: false,
-                },
-                {
-                    moduleName: 'invalid-semver',
-                    homepage: 'https://semver-none.com',
-                    regError: undefined,
-                    pkgError: undefined,
-                    latest: '0.0.62',
-                    installed: '0.0.62',
-                    isInstalled: true,
-                    notInstalled: false,
-                    packageJson: '^0.0.62',
-                    devDependency: false,
-                    usedInScripts: true,
-                    mismatch: false,
-                    semverValid: '6.1.9',
-                    easyUpgrade: false,
-                    bump: 'nonSemver',
-                    unused: false,
-                }];
             const currentStateSpy = {
                 get: sinon.stub().returns(fakePackages),
             };
@@ -138,7 +143,6 @@ describe('dependendencies', () => {
         afterEach(() => {
             deps = null;
             checkerStub = null;
-            fakePackages = null;
         });
         it('returns valid dependency object', () => {
             expect(Object.entries(deps.upgradable[0]).length).to.eql(9);
