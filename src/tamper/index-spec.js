3162c57279abdfcc660452b4ba86d712912b46c9
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-08-24 17:18:16 -0400

    fix(core): simplify tamper stub and related test

---
+++ src/tamper/index-spec.js
@@ -0,0 +1,16 @@
+import {
+    afterEach, beforeEach, describe, it,
+} from 'mocha';
+import { expect } from 'chai';
+import tamper from './index.js';
+
+describe('tamper', () => {
+
+    beforeEach(() => {
+    });
+    afterEach(() => {
+    });
+    it('TODO', () => {
+        expect(1).to.eql(1);
+    });
+});
--- src/tamper/index.js
+++ src/tamper/index.js
@@ -1,7 +1,7 @@
 import out from '../out/index.js';
 
 const start = (options) => {
-    if (options.recon) return options;
+    
 };
 
 export default {
