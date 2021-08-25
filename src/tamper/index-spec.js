d560425574b5bcdf731244ab4af2aebb2d64e198
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-08-25 13:48:55 -0400

    refactor(common): linting / cleanup

--- src/tamper/index-spec.js
+++ src/tamper/index-spec.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/no-duplicates */
-import {
-    afterEach, beforeEach, describe, it,
-} from 'mocha';
+// import {
+//    afterEach, beforeEach, describe, it,
+// } from 'mocha';
 import chai from 'chai';
-import { expect } from 'chai';
+// import { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import tamper from './index.js';
+// import tamper from './index.js';
 
 chai.use(chaiAsPromised);
 
-describe('tamper', () => {
-});
+// describe('tamper', () => {
+// });
