12b9bff99d081d3b09c4b7e62ce9f06f3b433794
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-09-07 21:51:30 -0400

    refactor(tamper): use existing constant for file name

--- src/tamper/index.js
+++ src/tamper/index.js
@@ -3,11 +3,12 @@ import fs from 'fs';
 import util from 'util';
 import out from '../out/index.js';
 import common from '../common/index.js';
+import { FILES } from '../constants/index.js';
 
 const readFile = util.promisify(fs.readFile);
 
 const readPackageLock = async (filePath, fileReader = readFile) => {
-    const buffer = await fileReader(path.join(filePath, 'package-lock.json'), 'utf8');
+    const buffer = await fileReader(path.join(filePath, FILES.PACKAGELOCK), 'utf8');
     return buffer ? JSON.parse(buffer) : {};
 };
 
