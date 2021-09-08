9653b48834417a8b36321d9103c1e47eba4ee597
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2021-09-07 21:15:59 -0400

    fix(tamper): properly parse data to an object

--- src/tamper/index.js
+++ src/tamper/index.js
@@ -4,14 +4,18 @@ import util from 'util';
 import out from '../out/index.js';
 import common from '../common/index.js';
 
-const asyncReadFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile);
 
-const readPackageLock = (filePath, fileReader = asyncReadFile) => fileReader(path.join(filePath, 'package-lock.json'), 'utf8');
+const readPackageLock = async (filePath, fileReader = readFile) => {
+    const buffer = await fileReader(path.join(filePath, 'package-lock.json'), 'utf8');
+    return buffer ? JSON.parse(buffer.toString()) : {};
+};
 
 const start = (options) => {
     common.validateOptions(options, 'tamper')
-        .then(readPackageLock(options.path))
-        .catch(out.debug);
+        .then(() => readPackageLock(options.tamper[0]))
+        .then((data) => out.warn(data.name))
+        .catch(out.warn);
 };
 
 export default {
